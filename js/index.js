2b421d34a95247056c492f5b7f90a6b385d4b28d
guillescarcella <scarcellag@udesa.edu.ar>
2022-11-08 11:16:57 -0300

    api

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 api_key= '3d4602582547bc4afa8f74ef23bb1e57'
+api = `https://api.themoviedb.org/3/movie/550?api_key=${api_key}`
+
+fetch(api)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+    return data;
+  
+})
+.catch(function(error) {
+    return error;
+})
