6c8767f45132681e7eab14d5d1b405e2b341d00f
guillescarcella <scarcellag@udesa.edu.ar>
2022-11-10 12:08:03 -0300

    edite el js de index

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 api_key= '3d4602582547bc4afa8f74ef23bb1e57'
 let peliculasPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`
+let seriesPopulares = `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=en-US&page=1`
+let mejorCalificadas = `https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}&language=en-US&page=1`
 
 let cajaPeliculas = document.querySelector(".caja_peliculas")
+let cajaSeries = document.querySelector(".caja_series")
+let cajaMejorCalificadas = document.querySelector(".caja_mejorCalificadas")
+
 
 fetch(peliculasPopulares)
 .then(function(response) {
@@ -24,8 +29,60 @@ fetch(peliculasPopulares)
     }
     cajaPeliculas.innerHTML+=contenido
     return data;
+})
+.catch(function(error) {
+    return error;
+})
+
+fetch(seriesPopulares)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data.results)
+    series = data.results;
+    let contenido = ""
+    for (let i = 0; i < 5; i++) {
+        contenido  += 
+        `<article class="fondo">
+            <img src="./img/B99Cover copy.jpeg" width="200" height="250" alt="imagen b99">
+            <p class="textoindex"> <b>${series[i].name}</b> <br>
+            Fecha de estreno: ${series[i].first_air_date}</p>
+            <a href="./detail-serie.html" class="img">Ver Mas</a>
+        </article>`
+    
+    }
+    cajaSeries.innerHTML+=contenido
+    return data;
   
 })
 .catch(function(error) {
     return error;
 })
+
+fetch(mejorCalificadas)
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data.results)
+    mejorCalificadas = data.results;
+    let contenido = ""
+    for (let i = 0; i < 5; i++) {
+        contenido  += 
+        `<article class="fondo">
+            <img src="./img/B99Cover copy.jpeg" width="200" height="250" alt="imagen b99">
+            <p class="textoindex"> <b>${mejorCalificadas[i].name}</b> <br>
+            Fecha de estreno: ${mejorCalificadas[i].first_air_date}</p>
+            <a href="./detail-serie.html" class="img">Ver Mas</a>
+        </article>`
+    
+    }
+    cajaMejorCalificadas.innerHTML+=contenido
+    return data;
+  
+})
+.catch(function(error) {
+    return error;
+})
+
