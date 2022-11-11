a2404c784248cfebb544e4239ac1984975769807
taligajer <tgajer@udesa.edu.ar>
2022-11-11 11:08:23 -0300

    js generos

--- js/genres.js
+++ js/genres.js
@@ -1,23 +1,48 @@
 api_key= '3d4602582547bc4afa8f74ef23bb1e57'
-let generos = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}&language=en-US`
+let generosPeliculas = `https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}&language=en-US`
+let generosSeries =`https://api.themoviedb.org/3/genre/tv/list?api_key=${api_key}&language=en-US`
 
-let listaGenero = document.querySelector(".lista-generos")
-//todavia no funciona!!!
-fetch(generos)
+let listaPelicula = document.querySelector(".generoPelis")
+let listaSerie = document.querySelector(".generoSeries")
+
+//GENERO PELICULA
+fetch(generosPeliculas)
 .then(function(response){
 	return response.json();
 })
 .then(function(data){
 	console.log(data);
-    generos = data;
+    generos = data.genres;
     let contenido = ""
     for (let i = 0; i < 5; i++) {
         contenido  += 
-        `<li class="genero-fantasia">
+        `<li class="genero-terror">
             <a class="linkgenerosseries" href="detail-genres.html">${generos[i].name}</a>
         </li>`
     } 
-    listaGenero.innerHTML+=contenido
+    listaPelicula.innerHTML=contenido
+})
+.catch(function(error){
+	console.log('El error es: ' + error);
+})
+
+//GENERO SERIE
+fetch(generosSeries)
+.then(function(response){
+	return response.json();
+})
+.then(function(data){
+	console.log(data);
+    generos = data.genres;
+    let contenido = ""
+    for (let i = 0; i < 5; i++) {
+        contenido  += 
+        `<li class="genero-terror">
+            <a class="linkgenerosseries" href="detail-genres.html">${generos[i].name}</a>
+        </li>`
+        
+    } 
+    listaSerie.innerHTML=contenido
 })
 .catch(function(error){
 	console.log('El error es: ' + error);
