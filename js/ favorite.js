0e05a2918a7e7d2ae0e71c2c2093fd308d918be3
cosycka <cosycka@udesa.edu.ar>
2022-11-19 20:03:11 -0300

    cambie cosas del css

--- js/ favorite.js
+++ js/ favorite.js
@@ -13,7 +13,7 @@ let sectionSeries = document.querySelector('.sectionfavseries')
 // peliulas
 let contenidopeliculasfav = "";
 if (peliculasfavoritas == null || peliculasfavoritas.length == 0){
-    sectionPeliculas.innerHTML = `<p> No se agregaron peliculas a favoritos <p/>`
+    sectionPeliculas.innerHTML = `<article><p class= pnohayfav> No se agregaron series a favoritos <p/></article>`
 }
 else{
     for (let i = 0; i < peliculasfavoritas.length; i++) {
